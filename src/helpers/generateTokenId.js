c68c9e82e0f9b487c8e225e10668458194b8d0ec
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-06-29 14:54:48 -0300

    token generator with map instead o for loop

--- src/helpers/generateTokenId.js
+++ src/helpers/generateTokenId.js
@@ -1,18 +1,9 @@
 const generateTokenId = (value) => {
-
-    value = typeof value === 'number' && value > 0 ? value : false;
-    if (!value) return false;
-
+    
     const allowedChars = 'abcdefghijklmnopqrstuvwxyz1234567890-_';
-    let generatedToken = '';
-
-    for ( let i = 1; i <= value; i++ ) {
-        let randomString = allowedChars.charAt(Math.floor(Math.random() * allowedChars.length));
-        generatedToken += randomString;
-    };
-
-    return generatedToken;
+    return [...new Array(value)]
+                    .map( char => char = allowedChars.charAt(Math.floor(Math.random() * allowedChars.length)))
+                    .join('');
 };
-
 
 module.exports = generateTokenId;

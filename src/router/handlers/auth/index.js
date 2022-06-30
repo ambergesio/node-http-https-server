8c3e7e455f55cdc8ea324c5d19842535b98f5c68
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-06-30 14:05:52 -0300

    authLogin and checkroute modified

--- src/router/handlers/auth/index.js
+++ src/router/handlers/auth/index.js
@@ -6,24 +6,20 @@ const createToken = require('../../../helpers/createToken');
 const authLogin = (data, cb) => {
 
     if (!data.payload) return cb(400, { error: true, message: 'You must send a payload in order to continue'});
-
-    //returns error, user data and file name
     validateUser(data.payload, (error, userData, file) => {
 
         if (error) return cb( 400, { error: true, message: `Invalid user or password`} );
-        
         createToken(userData, (error, token) => {
             if (error) return cb(400, { error: true, message: 'Token could not be generated properly. Login again'});
             return cb( 200, { error: error, message: "Valid user, you may continue.", token });
         });
-
     });
 };
 
 const checkroute = (data, cb) => {
-    verifyToken(data, (error, datos) => {
-        if (error) return cb(400, { error: true, message: `An error occurred ${error}`});
-        return cb(200, { error: false, data: datos});
+    verifyToken(data, (error, tokenData) => {
+        if (error) return cb(400, { error: true, message: tokenData});
+        return cb(200, { error: false, data: tokenData});
     });
 }
 
